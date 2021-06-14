1ad34fd3d37d581dd9823ca7d5a8362d173b70cb
qalska <qalska7@gmail.com>
2021-06-14 23:09:48 +0300

    первоначальная разметка mainpage

---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="dark">
+        <b-navbar-nav>
+            <router-link tag="b-nav-item" to="/">Главная</router-link>
+
+            <b-nav-item-dropdown text="Избранное" right>
+                <!-- <b-dropdown-item href="#">EN</b-dropdown-item> -->
+            </b-nav-item-dropdown>
+
+         </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .router-link {
+    text-decoration: none;
+  }
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1 class="text-white text-center mt-4">Weather Forecast</h1>
+    <b-form class="d-flex justify-content-center mt-5 mb-5">
+      <b-col sm="6">
+        <b-form-input
+          v-model="searchValue"
+          :type="text"
+          size="lg"
+          placeholder="Введите название города">
+        </b-form-input>
+      </b-col>
+    </b-form>
+    <div class="d-flex justify-content-center mt-5 mb-5">
+      <b-col sm="7">
+        <b-card bg-variant="dark" 
+          text-variant="white" 
+          border-variant="secondary"
+          align="center">
+          <b-card-title>Погода в <cite>Source Title</cite></b-card-title>
+          <b-card-text class="mt-3">
+            This card has supporting text below as a natural lead-in to additional content.
+          </b-card-text>
+        </b-card>
+      </b-col>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchValue: '',
+      weather: {
+        cityName: 'Санкт-Петербург',
+        country: 'Россия',
+        temperature: 18,
+        description: 'Облачно',
+        minTemp: '14',
+        maxTemp: '22',
+        fellsLike: '16',
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
