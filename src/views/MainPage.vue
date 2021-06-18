82b14820704de7df1d6156c5ab642fccc5d3e085
qalska <qalska7@gmail.com>
2021-06-18 23:19:31 +0300

    перенесла поиск в mainpage и изменила размер

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,20 +1,19 @@
 <template>
-  <div>
-    <v-row align="center"
-      justify="center">
-      <div>
-        <h1 class="mt-6">Weather Forecast</h1>
+  <div class="main-page">
+      <v-row align="center"
+        justify="center">
+        <v-col cols="7">
+            <h1 class="main-page__h2 mt-6 text-center">Weather Forecast</h1>
 
-        <v-col cols="20">
-          <v-autocomplete
-            v-model="autocompleteValue"
-            :items="cities"
-            label="Введите город">
-          </v-autocomplete>
+            <v-autocomplete
+              v-model="autocompleteValue"
+              :items="cities"
+              label="Введите город">
+            </v-autocomplete>
+
         </v-col>
+      </v-row>
 
-      </div>
-    </v-row>
 
     <forecast-card />
   </div>
@@ -24,9 +23,19 @@
 import ForecastCard from "../components/ForecastCard.vue"
 
 export default {
-  components: { ForecastCard }
+  components: { ForecastCard },
+  data() {
+    return {
+      cities: ['asd', 'qwe', '123'],
+      autocompleteValue: null,
+    }
+  }
 }
 </script>
 
 <style scoped>
+  .main-page__h2{
+    font-size: 40px;
+    letter-spacing: 0.25rem;
+  }
 </style>
